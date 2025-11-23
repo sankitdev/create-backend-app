aa65c232e86fecb491728d8303e3b19e9f17edff
sankitdev <ankitsingh635101@gmail.com>
2025-11-23 15:04:46 +0530

    refactor: simplify findByEmail method by removing error handling and logger

--- templates/express/src/services/user.service.ts
+++ templates/express/src/services/user.service.ts
@@ -1,5 +1,4 @@
 import { IUser, User } from "../models/User.model";
-import { logger } from "../utils/logger";
 import { BaseService } from "./base.service";
 
 export class UserService extends BaseService<IUser> {
@@ -8,15 +7,6 @@ export class UserService extends BaseService<IUser> {
   }
 
   public async findByEmail(email: string) {
-    try {
-      const user = this.model.findOne({ email });
-      if (!user) {
-        throw new Error("User not found");
-      }
-      return user;
-    } catch (error) {
-      logger.error({ error, email }, "Error fetching user by email");
-      throw error;
-    }
+    return this.model.findOne({ email });
   }
 }

394c3bf0ea6e2d124be0f84dc59744a29bb2b649
Ankit Singh <ankitsingh635101@gmail.com>
2025-12-03 20:35:48 +0530

    feat: add interactive project name prompt and update dependencies

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,6 +1,7 @@
 import { Command } from "commander";
 import { fileURLToPath } from "url";
 import path from "path";
+import { isCancel, cancel, text } from "@clack/prompts";
 import { scaffold } from "../utils/scaffold.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,8 +14,22 @@ program
     "Scaffold a backend project in right way without breaking a sweat"
   )
   .version("0.1.0")
-  .argument("[project-name]", "Name of the project directory", ".")
+  .argument("[project-name]", "Name of the project directory")
   .action(async (projectName) => {
+    // If projectName is missing → ask interactively
+    if (!projectName) {
+      projectName = await text({
+        message: "What is your project named?",
+        placeholder: "my-backend-app",
+        defaultValue: "my-backend-app",
+      });
+
+      if (isCancel(projectName)) {
+        cancel("Operation cancelled.");
+        process.exit(0);
+      }
+    }
+
     const templateDir = path.join(
       __dirname,
       "..",
@@ -22,6 +37,7 @@ program
       "templates",
       "express"
     );
+    
     await scaffold(projectName, templateDir);
   });
 
